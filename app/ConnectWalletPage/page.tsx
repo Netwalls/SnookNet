ac813268c678fb03a3fbd551f3287f11d025850c
chiscookeke11 <chiscookeke11@gmail.com>
2025-03-23 18:20:49 +0100

    wallett connect functionality

---
+++ app/ConnectWalletPage/page.tsx
@@ -0,0 +1,10 @@
+
+import WalletConnectButton from '../components/WalletConnectButton';
+
+export default function ConnectWalletPage() {
+    return (
+       <div className=' h-screen w-full flex items-center justify-center ' >
+         <WalletConnectButton/>
+       </div>
+    )
+}
