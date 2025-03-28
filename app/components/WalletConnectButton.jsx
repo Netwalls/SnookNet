71066fac6d96343838b2cd891e46eda98a8aa974
uyoxy <blessingtimothy20171@gmail.com>
2025-03-28 02:31:56 +0100

    conflict resolved

--- app/components/WalletConnectButton.jsx
+++ app/components/WalletConnectButton.jsx
@@ -1,12 +1,14 @@
-"use client"
+"use client";
 
 import React, { useState } from "react";
 import { connect, disconnect } from "get-starknet";
 import { encode } from "starknet";
+
 function WalletConnectButton() {
   const [walletAddress, setWalletAddress] = useState("");
   const [walletName, setWalletName] = useState("");
-  const [wallet, setWallet] = useState("");
+  const [wallet, setWallet] = useState<any>("");
+
   const handleDisconnect = async () => {
     await disconnect({ clearLastWallet: true });
     setWallet("");
@@ -14,9 +16,13 @@ function WalletConnectButton() {
     setWalletName("");
   };
 
-
-  const truncatedAdress =  walletAddress.slice(0, 5) + "..." + walletAddress.slice(10, 15) +  walletAddress.slice(20, 25)
-
+  const truncatedAddress =
+    walletAddress.length > 25
+      ? walletAddress.slice(0, 5) +
+        "..." +
+        walletAddress.slice(10, 15) +
+        walletAddress.slice(20, 25)
+      : walletAddress;
 
   const handleConnect = async () => {
     try {
@@ -34,32 +40,35 @@ function WalletConnectButton() {
       setWalletAddress(addr);
       setWalletName(getWallet?.name || "");
     } catch (e) {
-      // Handle user rejection to install MetaMask / the Starknet Snap.
       console.log(e);
     }
   };
+
   return (
     <div>
       {!walletAddress && (
-        <button onClick={handleConnect} className="relative bg-white transform -skew-x-12 cursor-pointer px-4 sm:px-8 h-[40px] sm:h-[62px]">
-        <span className="text-[#222C38] text-sm sm:text-base font-bold transform skew-x-12">
-          CONNECT WALLET
-        </span>
-      </button>
+        <button
+          onClick={handleConnect}
+          className="relative bg-white transform -skew-x-12 cursor-pointer px-4 sm:px-8 h-[40px] sm:h-[62px]"
+        >
+          <span className="text-[#222C38] text-sm sm:text-base font-bold transform skew-x-12">
+            CONNECT WALLET
+          </span>
+        </button>
       )}
       {walletAddress && (
         <div>
-          <button className="cursor-pointer flex flex-col items-center justify-center " onClick={handleDisconnect}>
-          <span>  Disconnect Wallet </span>
-
-            <p> {truncatedAdress} </p>
-            </button>
+          <button
+            className="cursor-pointer flex flex-col items-center justify-center"
+            onClick={handleDisconnect}
+          >
+            <span>Disconnect Wallet</span>
+            <p>{truncatedAddress}</p>
+          </button>
         </div>
       )}
-
-
-
     </div>
   );
 }
+
 export default WalletConnectButton;
