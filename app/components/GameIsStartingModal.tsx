0be84b6f39bb9ff6fad4d67f7e101b651c903038
Ezeh20 <ranter1570@gmail.com>
2025-03-29 17:49:36 +0100

    ..

---
+++ app/components/GameIsStartingModal.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const GameIsStartingModal = ({ isOpen }: { isOpen: boolean }) => {
+	if (!isOpen) return null;
+
+	const borderStyle = {
+		borderWidth: "2px 0px 2px 0px",
+		borderStyle: "solid",
+		borderImage:
+			"linear-gradient(90deg, rgba(169, 169, 169, 0) 14.47%, #488B92 50.31%, rgba(169, 169, 169, 0) 86.21%) 1",
+	};
+
+	return (
+		<div className="fixed inset-0 flex items-center justify-center bg-black/30 bg-opacity-50 backdrop-blur-[1px]">
+			<div
+				className="relative w-full px-8 py-[38px] text-white bg-opacity-70  border-y border-gray-600"
+				style={{
+					...borderStyle,
+					background:
+						"linear-gradient(90deg, rgba(59, 99, 108, 0) 1.5%, #3d213892 60.42%, rgba(59, 99, 108, 0) 86.39%)",
+				}}
+			>
+				<p className="text-[32px] text-[#F3F5FF] font-black text-center">
+					GAME IS STARTING...
+				</p>
+			</div>
+		</div>
+	);
+};
+
+export default GameIsStartingModal;
--- app/joingame/page.tsx
+++ app/joingame/page.tsx
@@ -1,38 +1,53 @@
 import Image from "next/image";
 import React from "react";
+import GameIsStartingModal from "../components/GameIsStartingModal";
 
 const JoinGame = () => {
-  
-  return (
-    <section className="flex justify-items-center relative min-h-screen pb-20 gap-16 p-2 font-[family-name:var(--font-geist-sans)] bg-[url('/background.svg')] bg-no-repeat bg-cover">
-      <button className=" bg-[#222C38] absolute right-10  transform -skew-x-12 cursor-pointer px-4 sm:px-8 h-[40px] sm:h-[62px]">
-          <span className=" text-sm text-white sm:text-base font-bold transform skew-x-12">
-            CONNECT WALLET
-          </span>
-        </button>
-     <div className="flex items-center gap-x-12 max-w-3/4 mx-auto mt-30">
-     <div className="flex flex-col gap-y-16">
-        <h1 className="font-[family-name:var(--font-geist-sans)] font-extrabold text-3xl">ENTER GAME CODE</h1>
-        <div className="flex gap-x-2  ">
-          <input type="text"  className="bg-[#222C38] border-[#696969] border-2 h-[4rem] w-[4rem] p-2  transform -skew-x-8"/> 
-          <input type="text"   className="bg-[#222C38] border-[#696969] border-2 h-[4rem] w-[4rem] p-2  transform -skew-x-8"/>
-          <input type="text"  className="bg-[#222C38] border-[#696969] border-2 h-[4rem] w-[4rem] p-2  transform -skew-x-8"/>
-          <input type="text"  className="bg-[#222C38] border-[#696969] border-2 h-[4rem] w-[4rem] p-2  transform -skew-x-8"/>
-        </div>
-        <div>
-        <button className="relative   bg-[#222C38] transform -skew-x-12 cursor-pointer px-4 sm:px-8 h-[40px] sm:h-[62px]">
-          <span className=" text-sm text-white sm:text-base font-bold transform skew-x-12">
-            JOIN GAME
-          </span>
-        </button>
-        </div>
-      </div>
-      <div>
-        <Image  src="/card.svg" alt="join card" width={300} height={500}/>
-      </div>
-     </div>
-    </section>
-  );
+	return (
+		<section className="flex justify-items-center relative min-h-screen pb-20 gap-16 p-2 font-[family-name:var(--font-geist-sans)] bg-[url('/background.svg')] bg-no-repeat bg-cover">
+			<button className=" bg-[#222C38] absolute right-10  transform -skew-x-12 cursor-pointer px-4 sm:px-8 h-[40px] sm:h-[62px]">
+				<span className=" text-sm text-white sm:text-base font-bold transform skew-x-12">
+					CONNECT WALLET
+				</span>
+			</button>
+			<div className="flex items-center gap-x-12 max-w-3/4 mx-auto mt-30">
+				<div className="flex flex-col gap-y-16">
+					<h1 className="font-[family-name:var(--font-geist-sans)] font-extrabold text-3xl">
+						ENTER GAME CODE
+					</h1>
+					<div className="flex gap-x-2  ">
+						<input
+							type="text"
+							className="bg-[#222C38] border-[#696969] border-2 h-[4rem] w-[4rem] p-2  transform -skew-x-8"
+						/>
+						<input
+							type="text"
+							className="bg-[#222C38] border-[#696969] border-2 h-[4rem] w-[4rem] p-2  transform -skew-x-8"
+						/>
+						<input
+							type="text"
+							className="bg-[#222C38] border-[#696969] border-2 h-[4rem] w-[4rem] p-2  transform -skew-x-8"
+						/>
+						<input
+							type="text"
+							className="bg-[#222C38] border-[#696969] border-2 h-[4rem] w-[4rem] p-2  transform -skew-x-8"
+						/>
+					</div>
+					<div>
+						<button className="relative   bg-[#222C38] transform -skew-x-12 cursor-pointer px-4 sm:px-8 h-[40px] sm:h-[62px]">
+							<span className=" text-sm text-white sm:text-base font-bold transform skew-x-12">
+								JOIN GAME
+							</span>
+						</button>
+					</div>
+				</div>
+				<div>
+					<Image src="/card.svg" alt="join card" width={300} height={500} />
+				</div>
+			</div>
+			<GameIsStartingModal isOpen={true}/>
+		</section>
+	);
 };
 
 export default JoinGame;
